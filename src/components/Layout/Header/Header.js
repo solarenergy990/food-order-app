2dade3f4686e9364a58d0562017ffb5ceacb8703
Eugen Soliar <eugen.soliar@exxeta.com>
2022-05-25 14:50:12 +0200

    creates CartIcon component

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="table full of delicious food" />
